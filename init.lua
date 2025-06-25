ffa7245ade05b298e7b4a07e4887ac8635ff5ccb
Chaim Halbert <chaim.leib.halbert@gmail.com>
2025-06-24 23:22:25 -0700

    fix config for mason-lspconfig

--- init.lua
+++ init.lua
@@ -1035,23 +1035,8 @@ require('lazy').setup {
         }
 
         require('mason-lspconfig').setup {
-          automatic_installation = true,
           ensure_installed = ensure_installed,
-          handlers = {
-            function(server_name)
-              local server = servers[server_name] or {}
-              -- This handles overriding only values explicitly passed
-              -- by the server configuration above. Useful when disabling
-              -- certain features of an LSP (for example, turning off formatting for ts_ls)
-              server.capabilities = vim.tbl_deep_extend(
-                'force',
-                {},
-                capabilities,
-                server.capabilities or {}
-              )
-              require('lspconfig')[server_name].setup(server)
-            end,
-          },
+          automatic_enable = true,
         }
       end,
     },
