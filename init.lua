9ffd1bbcff7dcabb60a7a40df126721961da147c
Chaim Halbert <chaim.leib.halbert@gmail.com>
2025-08-07 14:50:26 -0700

    feat: mdx syntax highlighting

--- init.lua
+++ init.lua
@@ -231,6 +231,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup {
   spec = {
     -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+    'jxnblk/vim-mdx-js', -- MDX syntax highlighting
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
     {
