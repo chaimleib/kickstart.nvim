d79f6a7727d8f3094fac29277828c207166ff676
Chaim Halbert <chaim.leib.halbert@gmail.com>
2026-04-28 16:53:18 -0700

    fix(whichkey): some default key prefixes conflict with actual commands

--- init.lua
+++ init.lua
@@ -662,11 +662,9 @@ require('lazy').setup {
         spec = {
           { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
           { '<leader>d', group = '[D]ocument' },
-          { '<leader>r', group = '[R]ename' },
           { '<leader>s', group = '[S]earch' },
           { '<leader>w', group = '[W]orkspace' },
           { '<leader>t', group = '[T]oggle' },
-          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         },
       },
     },
